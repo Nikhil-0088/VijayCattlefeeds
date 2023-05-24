aec1fc56f4515c679b144dc15fb0571473be2dcf
Nikhil <98630750+Nikhil-0088@users.noreply.github.com>
2023-05-24 11:37:43 +0530

    Update conn.js
    update conn string

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://nikhilb1216:Jayasri0088@cluster0.tslyqja.mongodb.net/",{
+mongoose.connect("mongodb+srv://nikhil_0088:Jayasri0088@cluster0.yv2qcze.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
